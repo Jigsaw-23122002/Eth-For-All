f3f7a7f24d8493303ec585ade0a5339168da61ae
Smit <smitsekhadia99@gmail.com>
2023-02-13 20:41:25 +0530

    added required plugins in hardhat.config for upgradable contracts

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
